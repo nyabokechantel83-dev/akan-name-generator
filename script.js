147b15efb0da6701225c4c2d9413fce6edaa1de9
nyabokechantel83-dev <nyabokechantel83@gmail.com>
2026-04-28 06:33:45 +0300

    add the akan names and the formula description

---
+++ script.js
@@ -0,0 +1,9 @@
+// Akan Names 
+// The formula gives // Akan names mapped to what the formula actually output
+// The formula gives: 0=Thu, 1=Fri, 2=Sat, 3=Sun, 4=Mon, 5=Tue, 6=Wed
+
+const maleNames   = ["Yaw", "Kofi", "Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku"];
+
+const femaleNames = ["Yaa", "Afua", "Ama", "Akosua", "Adwoa", "Abenaa", "Akua"];
+
+const dayNames    = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"];
